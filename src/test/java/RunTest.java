8f32954dac79e42d5e544f4af08a14e58e74be08
”Arina <kiruhinaarina@gmail.com>
2024-09-08 18:23:24 +0400

    push теста с ветки hotfix

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,21 +1,22 @@
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 
 public class RunTest {
 //its RunTest origin
     @BeforeEach
-    void before(){
+    void beforeHotfix(){
         System.out.println("Всем приготовится к сканированию!");
         }
 
     @Test
-    void test(){
+    void testHotfix(){
         if(6%3==0){
             System.out.println("Всё ок!");
         }
     }
 
     @AfterEach
-    void after (){
+    void afterHotfix(){
         if(6%16==0){
             System.out.println("Всё ок!");}
         else
