e628a8a5cf254b3d49a5045399f529d58f41360b
”Arina <kiruhinaarina@gmail.com>
2024-09-08 17:39:38 +0400

    push теста с ветки develop

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,6 +1,22 @@
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class RunTest {
 //its RunTest origin
-public static void main(String[] args) {
-System.out.println("Yoy!");
-}
+    @BeforeEach
+    public void before (){
+        System.out.println("Всем приготовится!");
+    }
+    @Test
+    public void test(){
+        Assertions.assertTrue(3>2,"Ошибка проверки");
+
+    }
+    @AfterEach
+    public void after(){
+        Assertions.assertFalse(3>2,"Ошибка проверки");
+    }
 }
